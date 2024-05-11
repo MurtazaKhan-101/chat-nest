8b273fbdc17ba04b245376bef40df4d1e574c0d1
MurtazaKhan-101 <murtazakhan1617@gmail.com>
2024-05-11 23:45:29 +0500

    new fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
 import ActiveStatus from "./componenets/ActiveStatus";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
